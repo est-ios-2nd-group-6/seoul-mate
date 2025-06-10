31e7bb1612a6e876c627db5fd6f0d9815c5a7315
김현식 <nnopdev@gmail.com>
2025-06-11 05:40:06 +0900

    feat:서치 서치결과 테이블뷰의 헤더 및 커스텀얼렛시트

--- SeoulMate/SeoulMate/Source/Modules/Search/SearchViewController.swift
+++ SeoulMate/SeoulMate/Source/Modules/Search/SearchViewController.swift
@@ -9,59 +9,68 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
-    var tags = ["오사카","제주","다낭","파리","도쿄","부산","방콕","다낭","괌","삿포로",]
-    
+    var tags = ["오사카", "제주", "다낭", "파리", "도쿄", "부산", "방콕", "다낭", "괌", "삿포로"]
+
     @IBOutlet weak var searchResultTableView: UITableView!
-    
+
     @IBOutlet weak var tagCollectionView: UICollectionView!
-    
+
     @IBOutlet weak var searchbarView: UISearchBar!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-//        searchResultTableView.dataSource = self
-//        searchResultTableView.showsVerticalScrollIndicator = false
-        
+        searchResultTableView.dataSource = self
+        searchResultTableView.delegate = self
+        searchResultTableView.showsVerticalScrollIndicator = false
+
         searchbarView.setImage(UIImage(), for: .search, state: .normal)
         searchbarView.becomeFirstResponder()
-        
+
         tagCollectionView.dataSource = self
         tagCollectionView.delegate = self
-        
+
         let layout = LeftAlignedCollectionViewFlowLayout()
         layout.minimumLineSpacing = 3
         layout.minimumInteritemSpacing = 3
         layout.sectionInset = UIEdgeInsets(top: 5, left: 2, bottom: 5, right: 3)
         tagCollectionView.collectionViewLayout = layout
         tagCollectionView.backgroundColor = .systemBackground
-        
+
     }
-    
 }
 
-extension SearchViewController : UICollectionViewDataSource {
-    
+extension SearchViewController: UICollectionViewDataSource {
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return tags.count
     }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: TagCollectionViewCell.self), for: indexPath) as! TagCollectionViewCell
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell =
+            collectionView.dequeueReusableCell(
+                withReuseIdentifier: String(describing: TagCollectionViewCell.self),
+                for: indexPath
+            ) as! TagCollectionViewCell
         cell.setCell(text: tags[indexPath.item])
         print(#function)
         return cell
     }
 }
 
-extension SearchViewController : UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
         let text = tags[indexPath.item]
-        let size = (text as NSString).size(withAttributes: [.font:UIFont.systemFont(ofSize: 14)])
-        return CGSize(width: size.width+24, height: size.height+16)
+        let size = (text as NSString).size(withAttributes: [.font: UIFont.systemFont(ofSize: 14)])
+        return CGSize(width: size.width + 24, height: size.height + 16)
     }
 }
 
-extension SearchViewController : UICollectionViewDelegate {
+extension SearchViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print(tags[indexPath.row])
     }
@@ -87,14 +96,52 @@ class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
     }
 }
 
-extension SearchViewController : UITableViewDataSource {
+extension SearchViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing:SearchResultTableViewCell.self), for: indexPath) as! SearchResultTableViewCell
-        return cell
+        switch indexPath.row {
+        case 0:
+            let cell =
+                tableView.dequeueReusableCell(withIdentifier: String(describing: SearchResultTableHeaderCell.self))
+                as! SearchResultTableHeaderCell
+            cell.delegate = self
+            return cell
+        default:
+            let cell =
+                tableView.dequeueReusableCell(
+                    withIdentifier: String(describing: SearchResultTableViewCell.self),
+                    for: indexPath
+                ) as! SearchResultTableViewCell
+            return cell
+        }
+        //        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing:SearchResultTableViewCell.self), for: indexPath) as! SearchResultTableViewCell
+        //        return cell
     }
-    
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
+
+protocol SearchViewControllerDelegate: AnyObject {
+    func didButtonTapped(cell: SearchResultTableHeaderCell)
+}
+
+extension SearchViewController: SearchViewControllerDelegate {
+    func didButtonTapped(cell: SearchResultTableHeaderCell) {
+        let alertVC = CustomAlertController()
+        alertVC.delegate = self
+        present(alertVC, animated: false)
+    }
+}
+
+extension SearchViewController: CustomAlertControllerDelegate {
+    func deleteRecentItem(_ alert: CustomAlertController) {
+        self.tags.removeAll()
+        self.tagCollectionView.reloadData()
+    }
 }

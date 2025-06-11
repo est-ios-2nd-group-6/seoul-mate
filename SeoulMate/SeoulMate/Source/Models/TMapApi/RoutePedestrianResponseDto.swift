c1b15b71c4aab83908b76c1096ffdbe3facc471f
성주현 <qpqpqoqo12@gmail.com>
2025-06-11 22:53:27 +0900

    feat: T Map Api 도보 경로 ResponseDto 생성

---
+++ SeoulMate/SeoulMate/Source/Models/TMapApi/RoutePedestrianResponseDto.swift
@@ -0,0 +1,81 @@
+//
+//  RoutePedestrianResponseDto.swift
+//  SeoulMate
+//
+//  Created by JuHyeon Seong on 6/10/25.
+//
+
+import Foundation
+
+struct RoutePedestrianResponseDto: Codable {
+    let type: String
+    let features: [Feature]
+}
+
+struct Feature: Codable {
+    let type: FeatureType
+    let geometry: Geometry
+    let properties: Properties
+}
+
+struct Geometry: Codable {
+    let type: GeometryType
+    let coordinates: [Coordinate]
+}
+
+enum Coordinate: Codable {
+    case double(Double)
+    case doubleArray([Double])
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let x = try? container.decode([Double].self) {
+            self = .doubleArray(x)
+            return
+        }
+        if let x = try? container.decode(Double.self) {
+            self = .double(x)
+            return
+        }
+        throw DecodingError.typeMismatch(Coordinate.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for Coordinate"))
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .double(let x):
+            try container.encode(x)
+        case .doubleArray(let x):
+            try container.encode(x)
+        }
+    }
+}
+
+enum GeometryType: String, Codable {
+    case lineString = "LineString"
+    case point = "Point"
+}
+
+struct Properties: Codable {
+    let totalDistance, totalTime: Int?
+    let index: Int
+    let pointIndex: Int?
+    let name, description: String
+    let direction, nearPoiName, nearPoiX, nearPoiY: String?
+    let intersectionName: String?
+    let facilityType, facilityName: String
+    let turnType: Int?
+    let pointType: PointType?
+    let lineIndex, distance, time, roadType: Int?
+    let categoryRoadType: Int?
+}
+
+enum PointType: String, Codable {
+    case ep = "EP"
+    case gp = "GP"
+    case sp = "SP"
+}
+
+enum FeatureType: String, Codable {
+    case feature = "Feature"
+}

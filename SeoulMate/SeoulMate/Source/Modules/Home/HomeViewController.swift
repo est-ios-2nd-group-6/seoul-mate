c0501a3bfbf67c5104ad446eaae776e1dccc48b6
성주현 <qpqpqoqo12@gmail.com>
2025-06-13 10:59:20 +0900

    feat: Main Tab View & Home View 임시 생성

--- SeoulMate/SeoulMate/Source/Modules/Home/HomeViewController.swift
+++ SeoulMate/SeoulMate/Source/Modules/Home/HomeViewController.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    @IBOutlet weak var rcmCourseListByAreaCollectionView: UICollectionView!
+
+    var rcmCourseListByArea: [RecommandCourse] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        rcmCourseListByArea = TourApiManager.shared.rcmCourseListByArea
     }
 }
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return rcmCourseListByArea.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecommandCourseCollectionViewCell", for: indexPath) as? RecommandCourseCollectionViewCell
+        else { return UICollectionViewCell() }
+
+        let course = rcmCourseListByArea[indexPath.item]
+
+        cell.thumbnailImageView.image = UIImage(named: "subdetailimg")
+        cell.titleLabel.text = course.title
+
+        return cell
+    }
+    
+
+}
+

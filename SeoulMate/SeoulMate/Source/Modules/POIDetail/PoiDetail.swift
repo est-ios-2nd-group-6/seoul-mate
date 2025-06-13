8011588be711c86220c41e7acdc97ac49b2d46b4
김현식 <nnopdev@gmail.com>
2025-06-14 05:01:55 +0900

    feat: PoiDetail 장소정보 셀 위치작업

--- SeoulMate/SeoulMate/Source/Modules/POIDetail/PoiDetail.swift
+++ SeoulMate/SeoulMate/Source/Modules/POIDetail/PoiDetail.swift
@@ -7,10 +7,31 @@
 
 import UIKit
 class PoiDetailViewController: UIViewController {
+    @IBOutlet weak var detailTableView: UITableView!
     @IBAction func dismissView(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        detailTableView.dataSource = self
+        detailTableView.delegate = self
+    }
+}
+
+extension PoiDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: PoiInfoCell.self)) as! PoiInfoCell
+        return cell
+    }
+}
+
+extension PoiDetailViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 380
     }
 }
---
+++ SeoulMate/SeoulMate/Source/Modules/POIDetail/PoiInfoCell.swift
@@ -0,0 +1,23 @@
+//
+//  PoiInfoCell.swift
+//  SeoulMate
+//
+//  Created by DEV on 6/14/25.
+//
+
+import UIKit
+
+class PoiInfoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

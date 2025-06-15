515486cffc18e52b5c08fe6742806b1ac5d9282e
성주현 <qpqpqoqo12@gmail.com>
2025-06-15 15:26:13 +0900

    chore: Route Map Cell 폴더 생성 및 경로 이동

---
+++ SeoulMate/SeoulMate/Source/Modules/RouteMap/cell/TransitDetailTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TransitDetailTableViewCell.swift
+//  SeoulMate
+//
+//  Created by JuHyeon Seong on 6/15/25.
+//
+
+import UIKit
+
+class TransitDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}

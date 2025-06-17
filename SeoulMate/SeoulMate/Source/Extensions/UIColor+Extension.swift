d9d9b18cf7d772a159283dfb357421bb4ed957f4
성주현 <qpqpqoqo12@gmail.com>
2025-06-17 19:43:32 +0900

    feat: 대중교통 경로 색상 업데이트

--- SeoulMate/SeoulMate/Source/Extensions/UIColor+Extension.swift
+++ SeoulMate/SeoulMate/Source/Extensions/UIColor+Extension.swift
@@ -9,29 +9,23 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    convenience init?(hexString: String) {
-        let r, g, b, a: CGFloat
+    convenience init?(hexString: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
 
-        if hexString.hasPrefix("#") {
-            let start = hexString.index(hexString.startIndex, offsetBy: 1)
-            let hexColor = String(hexString[start...])
-
-            if hexColor.count == 8 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-
-                    self.init(red: r, green: g, blue: b, alpha: a)
-                    return
-                }
-            }
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
         }
 
-        return nil
+        if hexFormatted.count != 6 {
+			return nil
+        }
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
     }
 }
